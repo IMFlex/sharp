26edbd81624818e52270e0b317585998d5f5065c
IMFlex <forgithub@zippiex.com>
2018-09-12 08:39:30 +0300

    first commit

---
+++ Program_5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Зарплата
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            float z = Convert.ToSingle(Console.ReadLine());
+            float god = z * 12;
+            float den =z / 30;
+            float hour =(z / 30) / 24;
+            float min = ((z / 30) / 24) / 60;
+            double ned=z/4.285714285714286;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Зарплата за год" + ":" +" "+(int)god);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Зарплата за неделю"+":" + " " + (int)ned);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Зарплата за сутки" + ":" + " " + (int)den);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Зарплата за час" + ":" + " " + (int)hour);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Зарплата за минуту" + ":" + " " + (Single)min);
+            Console.ReadLine();
+        }
+    }
+}

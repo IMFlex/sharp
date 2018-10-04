055ce53ed86ee4f37f87256ae863f61b4128ebbb
IMFlex <forgithub@zippiex.com>
2018-10-04 17:58:49 +0300

    Continuation

---
+++ Задание А(отборочные 2016).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A.Задача_отборочные_2016г._
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int s = Convert.ToInt32(Console.ReadLine());
+             int v1 = Convert.ToInt32(Console.ReadLine());
+             int v2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кол-во секунд" + " " + s / ((v1 + v2) / 3.6));
+            Console.ReadLine();
+        }
+    }
+}

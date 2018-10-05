fd8ae21c1f44d2fd071acc2a33203e290a9bb9d8
IMFlex <forgithub@zippiex.com>
2018-10-05 21:56:50 +0300

    Continue to blunt

---
+++ B.Палиндром(Отборочные 2016).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B.Палиндром_Отборочные_2016г._
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string a = Convert.ToString(Console.ReadLine());
+            for (int b = a.Length; b > 0; b--) //7
+            {
+                if (b > a.Length) break;
+                Console.WriteLine(a[b - 1]);
+            }
+            Console.ReadKey();
+        }
+
+
+        
+        
+    }
+}
